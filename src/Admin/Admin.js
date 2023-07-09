cb1e4fb46b6f57b9dab651f4f26e7ba1093232e7
Mary Williams <sbyardnss@gmail.com>
2023-07-09 17:00:01 -0500

    adding cloudinary to admin side

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom"
 
 export const Admin = () => {
-
+    const navigate = useNavigate();
+    // let cloudinary = require('cloudinary').v2;
+    // cloudinary.config({
+    //     cloud_name: 'da0za1x54',
+    //     api_key: '863311724842548',
+    //     api_secret: 'QGcwrKF--ZvrN0_0qPl4TgLJ1aY'
+    // });
     return <>
-        <h2>this is the admin page</h2>
+        <main id="adminContainer">
+            <h2>this is the admin page</h2>
+            <button onClick={() => {
+                localStorage.removeItem("carsello_user")
+                navigate("/", { replace: true })
+            }}>logout</button>
+            <section id="addArtSection">
+
+            </section>
+            <section id="addEventSection">
+
+            </section>
+        </main>
+
     </>
 }
