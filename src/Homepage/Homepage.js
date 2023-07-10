4d2e96e5abe955221a79a8d47d3327f264cb5796
Mary Williams <sbyardnss@gmail.com>
2023-07-10 18:08:20 -0500

    updated layout of homepage to reflect figma design. need copy from lizzie

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,15 +1,53 @@
-import { getArt } from "../ServerManager"
+import { getArt, getEvents } from "../ServerManager"
 import { useContext, useEffect, useState } from "react"
 import { Navigate, useNavigate } from "react-router-dom"
 import "../Homepage/Homepage.css"
 export const Homepage = () => {
     const navigate = useNavigate()
-
+    const [events, setEvents] = useState([])
+    useEffect(
+        () => {
+            getEvents()
+                .then(data => setEvents(data))
+        }, []
+    )
     return <>
-        <main>
-            <h2>Carsello Homepage</h2>
+        <main id="homepageContainer">
             <section id="servicesOfferedList">
-
+                <h2>need copy here for intro to offerings</h2>
+                <div className="offeredServiceLi">
+                    <h3>Art Commission</h3>
+                    <p>description</p>
+                </div>
+                <div className="offeredServiceLi">
+                    <h3>Art Curation</h3>
+                    <p>description</p>
+                </div>
+                <div className="offeredServiceLi">
+                    <h3>Grant, Scholarship, and Copywriting services</h3>
+                    <p>description</p>
+                </div>
+                <div className="offeredServiceLi">
+                    <h3>Muraling, sign painting</h3>
+                    <p>description</p>
+                </div>
+            </section>
+            <section id="homepageEventsList">
+                <h3>upcoming events</h3>
+                {
+                    events.map(e => {
+                        const date = new Date(e.date_time).toDateString()
+                        const time = new Date(e.date_time).toLocaleTimeString('en-us', { hour: "numeric", minute: "numeric" })
+                        return (
+                            <div key={e.id} className="homeEventListItem">
+                                <div className="eventLITitle">{e.title}</div>
+                                <div className="eventLILogistics">{e.location}</div>
+                                <div className="eventLILogistics">{date}</div>
+                                <div className="eventLILogistics">{time}</div>
+                            </div>
+                        )
+                    })
+                }
             </section>
         </main>
     </>
