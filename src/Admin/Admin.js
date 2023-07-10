016f6ee9726f9f4a8c64829e4d0989cff1dc1137
Mary Williams <sbyardnss@gmail.com>
2023-07-10 15:00:46 -0500

    event form works. submission testing still needed

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -6,30 +6,46 @@ import { ArtForm } from "./ArtForm";
 import { getArt, getEvents, sendArt } from "../ServerManager";
 import { ArtList } from "../ArtList/ArtList";
 import { EventList } from "../EventList/EventList";
+import { EventForm } from "./EventForm";
 export const Admin = () => {
     const navigate = useNavigate();
     const [addArt, setAddArt] = useState(false)
-    const [imageUrl, setImageUrl] = useState("")
+    const [addEvent, setAddEvent] = useState(false)
+    const [artImageUrl, setArtImageUrl] = useState("")
     const [artwork, setArtwork] = useState([])
     const [viewArt, setViewArt] = useState(false)
     const [events, setEvents] = useState([])
     const [viewEvents, setViewEvents] = useState(false)
+    const [sendEvent, setSendEvent] = useState(false)
     const newTitle = useRef()
     const newPrice = useRef()
     const newYear = useRef()
+    const newEventTitle = useRef()
+    const newEventLocation = useRef()
+    const [newEventDateTime, setNewEventDateTime] = useState("")
+    const newEventPrice = useRef()
     const [newPiece, updateNewPiece] = useState({
         title: newTitle.current?.value,
         year: newYear.current?.value,
         image: '',
         price: newPrice.current?.value
     })
-
+    const [newEvent, updateNewEvent] = useState({
+        title: "",
+        location: "", 
+        dateTime: "",
+        image: "",
+        link: "",
+        details: "",
+        price: 0
+    })
+    
     useEffect(
         () => {
             const copy = { ...newPiece }
-            copy.image = imageUrl
+            copy.image = artImageUrl
             updateNewPiece(copy)
-        }, [imageUrl]
+        }, [artImageUrl]
     )
     useEffect(
         () => {
@@ -54,11 +70,11 @@ export const Admin = () => {
                     <div>
                         <label>image</label>
                         <UploadWidget
-                            urlSet={setImageUrl}
+                            urlSet={setArtImageUrl}
                             imageName={newTitle.current?.value} />
                     </div>
+                    <button onClick={() => setAddArt(false)}>cancel</button>
                     <button onClick={() => sendArt(newPiece)}>submit</button>
-
                 </section>
             )
         }
@@ -66,6 +82,52 @@ export const Admin = () => {
             return (
                 <section id="addArtSection">
                     <button onClick={() => setAddArt(true)}>add art</button>
+                </section>
+            )
+        }
+    }
+    useEffect(
+        () => {
+            if (newEventDateTime && sendEvent === true) {
+                const copy = {...newEvent}
+                copy.dateTime = new Date(newEventDateTime).toISOString()
+                console.log(copy)
+            }
+        },[sendEvent]
+    )
+    // useEffect(
+    //     () => {
+    //         console.log(newEvent)
+    //     },[newEvent]
+    // )
+    const addEventForm = () => {
+        if (addEvent) {
+            return (
+                <section id="addEventSection">
+                    <EventForm
+                        title={newEventTitle}
+                        location={newEventLocation} 
+                        dateTime={newEventDateTime}
+                        setDateTime={setNewEventDateTime}
+                        price={newEventPrice}
+                        event={newEvent}
+                        updateEvent={updateNewEvent}
+                    />
+                    {/* <div>
+                        <label>image</label>
+                        <UploadWidget
+                            urlSet={setArtImageUrl}
+                            imageName={newTitle.current?.value} />
+                    </div> */}
+                    <button onClick={() => setAddEvent(false)}>cancel</button>
+                    <button onClick={() => setSendEvent(true)}>submit</button>
+                </section>
+            )
+        }
+        else {
+            return (
+                <section id="addEventSection">
+                    <button onClick={() => setAddEvent(true)}>add event</button>
                 </section>
             )
         }
@@ -111,6 +173,7 @@ export const Admin = () => {
             }}>{viewArt === false ? 'View Art List' : 'Cancel'}</button>
             {addArtForm()}
             {artList()}
+            {addEventForm()}
             {eventList()}
         </main>
     </>
---
+++ src/Admin/EventForm.js
@@ -0,0 +1,114 @@
+import { useRef } from "react"
+
+
+export const EventForm = ({ title, location, dateTime, setDateTime, price, event, updateEvent }) => {
+    const date = useRef()
+    const time = useRef()
+    const handleChange = (e) => {
+        const copy = { ...event }
+        if (e.target.id === 'price') {
+            copy[e.target.id] = parseInt(e.target.value)
+            updateEvent(copy)
+        }
+        else if (e.target.id === 'date' || e.target.id === 'time') {
+            // if (e.target.id === 'time'){
+                const [timeHM, amPm]= time.current?.value.split(' ')
+                let [hour, minute] = timeHM.split(":")
+                if (amPm?.toLowerCase() === 'pm') {
+                    hour = parseInt(hour) + 12
+                    setDateTime(`${date.current.value} ${hour}:${minute}`)
+                }
+                else {
+                    setDateTime(`${date.current.value} ${hour}:${minute}`)
+                }
+            // }
+            
+        }
+        else {
+            copy[e.target.id] = e.target.value
+            updateEvent(copy)
+        }
+    }
+    // console.log(date.current.value)
+
+    // console.log(new Date ("2023-07-10 14:30"))
+    return <>
+        <section id="newEventForm">
+            <label className="form-labels" htmlFor="newTitle">Event Title</label>
+            <input
+                id="title"
+                onChange={handleChange}
+                className="form-input"
+                type="text"
+                ref={title}
+                placeholder="event title"
+            // required autoFocus
+            />
+            <label className="form-labels" htmlFor="newLocation">Event Location</label>
+            <input
+                id="location"
+                onChange={handleChange}
+                className="form-input"
+                type="text"
+                ref={location}
+                placeholder="event location"
+            />
+            <label className="form-labels" htmlFor="newPrice">Event Price</label>
+            <input
+                id="price"
+                onChange={handleChange}
+                className="form-input"
+                type="number"
+                ref={price}
+                required autoFocus
+            />
+            <label className="form-labels" htmlFor="eventDate">Event Date</label>
+            <input
+                id="date"
+                onChange={handleChange}
+                className="form-input"
+                type="text"
+                ref={date}
+                required autoFocus
+            />
+            <label className="form-labels" htmlFor="eventTime">Event Time</label>
+            <input
+                id="time"
+                onChange={handleChange}
+                className="form-input"
+                placeholder="hh:mm (pm/am)"
+                type="text"
+                ref={time}
+                required autoFocus
+            />
+            <label className="form-labels" htmlFor="eventLink">Event Link</label>
+            <input
+                id="link"
+                onChange={handleChange}
+                className="form-input"
+                placeholder="event url"
+                type="text"
+                required autoFocus
+            />
+            <label className="form-labels" htmlFor="eventImage">Event Image URL</label>
+            <input
+                id="image"
+                onChange={handleChange}
+                className="form-input"
+                placeholder="event image url"
+                type="text"
+                required autoFocus
+            />
+            <label className="form-labels" htmlFor="eventDetails">Event Details</label>
+            <input
+                id="details"
+                onChange={handleChange}
+                className="form-input"
+                placeholder="event details"
+                type="text"
+                required autoFocus
+            />
+            
+        </section>
+    </>
+}
