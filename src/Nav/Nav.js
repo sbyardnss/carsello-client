c324c8022c5be8ba3c7a163564ed295e58054209
Mary Williams <sbyardnss@gmail.com>
2023-08-04 16:49:57 -0500

    updated html for all and updated nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -14,9 +14,9 @@ export const Nav = () => {
                 <img id="carselloLogo" src={carselloLogo} onClick={()=> navigate("")}/>
                 {/* <Link id="carselloLogo" to="/">logo</Link> */}
                 <ul id="carselloNavigation">
-                    <li><Link className="navLink" to="/art">Art</Link></li>
-                    <li><Link className="navLink" to="/events">Events</Link></li>
-                    <li><Link className="navLink" to="/otherServices">Other Services</Link></li>
+                    <li><Link className="navLink" to="/art">art</Link></li>
+                    <li><Link className="navLink" to="/events">events</Link></li>
+                    <li><Link className="navLink" to="/otherServices">other services</Link></li>
                     <li><Link className="navLink" to="/admin">admin</Link></li>
                 </ul>
             </div>
