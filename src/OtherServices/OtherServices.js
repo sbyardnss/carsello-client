0640f460148ee13dc6d42a888515981cc85d5b7f
Mary Williams <sbyardnss@gmail.com>
2023-08-04 17:17:36 -0500

    updated curation page

--- src/OtherServices/OtherServices.js
+++ src/OtherServices/OtherServices.js
@@ -6,14 +6,26 @@ export const OtherServices = () => {
         <main id="otherServicesContainer">
             <section className="otherServicesHeader">
                 <img className="otherServicesImage" src={curationImage} />
-                <p className="serviceDescription">
-                    <div className="descriptionLine">I specialize in abstract and whimsical designs.</div>
-                    <div className="descriptionLine">I have made murals and/or flyers for Las Paletas, Xiao Bao,</div>
+                <p className="serviceDescription xxLargeFont">
+                    <div className="descriptionLine">I strive to curate accessible and adaptable art shows.</div>
+                    {/* <div className="descriptionLine">I integrate accessibility into my practice by: teaching artists about hanging, framing, and art placement, assisting with install and uninstall, accommodating all scheduling needs, and negotiating a mutually beneficial agreement between the artists and venue.</div>
+                    <div className="descriptionLine">Additionally, I seek artists with a variety of experience levels and love guiding folks through the process of their first show.</div>
+                    <div className="descriptionLine">I have curated art shows for Fido, Bongo Belmont, Game Point Cafe, and Planned Parenthood.</div> */}
+                    {/* <div className="descriptionLine">I specialize in abstract and whimsical designs.</div> */}
+                    {/* <div className="descriptionLine">I have made murals and/or flyers for Las Paletas, Xiao Bao,</div>
                     <div className="descriptionLine">Soho House, Fido, Bongo East, Bongo Belmont and more.</div>
                     <div className="descriptionLine">Pricing: 30-60/hr.</div>
                     <div className="descriptionLine">All pricing is sliding scale, but willing to work for select trade.</div>
-                    <div className="descriptionLine">Reach out via email with questions.</div>
+                    <div className="descriptionLine">Reach out via email with questions.</div> */}
                 </p>
+
+            </section>
+            <section className="otherServicesHeader">
+                <p className="serviceDescription">
+                    <div className="descriptionLine mediumFont">I integrate accessibility into my practice by: teaching artists about hanging, framing, and art placement, assisting with install and uninstall, accommodating all scheduling needs, and negotiating a mutually beneficial agreement between the artists and venue.</div>
+                </p>
+                <img className="otherServicesImage" src={curationImage} />
+
             </section>
         </main>
     </>
