7c411b6c5a4b2dce9f0bdcf37ca7ce66f6f14f57
Mary Williams <sbyardnss@gmail.com>
2023-08-05 17:39:43 -0500

    removed words from homepage

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -26,10 +26,9 @@ export const Homepage = () => {
                 <img id="brokenKeyBack" src={brokenKey} />
             </section>
             <article id="offeredServices">
-                <section id="selfDescription">
+                {/* <section id="selfDescription">
                     <p>nostalgia <br />familiarity<br />accessability <br /></p>
-                    {/* <p>I derive shape inspirations from natural elements and exaggerate them into a more abstract form, creating nostalgic and familiar images</p> */}
-                </section>
+                </section> */}
                 <section className="serviceSectionRight">
                     <img className="servicesImage" src={artImage} onClick={() => navigate('/art')} />
                     <div className="rotateLeftLabel">art</div>
