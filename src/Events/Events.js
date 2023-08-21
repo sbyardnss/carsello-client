7cd5600bbce8321d85c883f4741830709cfa9c05
Mary Williams <sbyardnss@gmail.com>
2023-08-21 10:08:38 -0500

    fixed nav sizing for mobile

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import "../Events/Events.css"
 import { getEvents } from "../ServerManager"
+import { Link } from "react-router-dom"
 
 export const Events = () => {
     const [events, setEvents] = useState([])
@@ -10,24 +11,30 @@ export const Events = () => {
                 .then(data => setEvents(data))
         }, []
     )
+    console.log(events)
     return <>
-        <main id="eventsContainer">
-            <div className="customFontHead">Upcoming Events</div>
+        <main id="serviceContainer">
+            <div className="eventsHeader">
+                <div className="customFontHead">Upcoming Events</div>
+            </div>
             <section id="eventsListContainer">
                 <ul id="eventsList">
                     {
                         events.map(e => {
-                            const date = new Date (e.date_time)
+                            const date = new Date(e.date_time)
                             const printedDate = date.toDateString()
-                            const time = date.toLocaleTimeString('en-us', {hour: 'numeric', minute: 'numeric'})
+                            const time = date.toLocaleTimeString('en-us', { hour: 'numeric', minute: 'numeric' })
                             return (
                                 <li className="eventsListItem opaqueCard" key={e.id}>
                                     <div className="eventInfo">
-                                        <div>{e.title}</div>
-                                        <div>{e.location}</div>
-                                        <div>{printedDate}</div>
-                                        <div>{time}</div>
-                                        <div>{e.details}</div>
+                                        <div className="mediumFont">{e.title}</div>
+                                        <div className="smallFont">
+                                            {e.location}
+                                            {printedDate}
+                                            {time}
+                                        </div>
+                                        <Link className="aquaHighlight" to={e.link}>See Event Page</Link>
+                                        <div className="eventDescription">{e.details}</div>
                                     </div>
                                     <img className="eventImage" src={e.image} />
                                 </li>
